d49d13c746abd9d6297b5ae4dbed001f8cf68cdd
TitularTea <titular@outlook.kr>
2026-07-30 20:33:28 +0900

    sssss

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './timeline/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
